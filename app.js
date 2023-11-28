625ab2f296330e466a74bbb453942cf2e489e2fe
Antonio Quaresma <tozequa@gmail.com>
2023-11-28 11:00:00 +0000

    adding start new game functionality

--- app.js
+++ app.js
@@ -57,6 +57,12 @@ const app = Vue.createApp({
     },
 
     methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+        },
 
         attackMonster() {
             this.currentRound++;
